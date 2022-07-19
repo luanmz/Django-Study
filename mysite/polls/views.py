532732f16e56ff31ec2e60cab5d16944b8ed0ae3
luanmz <luanpereiramz@gmail.com>
2022-07-19 10:17:36 -0300

    Progresso sendo realizado

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,23 @@
 from http.client import HTTPResponse
-from django.shortcuts import render
+from multiprocessing import context
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def details(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'polls/details.html', context)
+
+def results(request, question_id):
+    return HttpResponse(f'Esses são os resultados da pergunta de número {question_id}')
+
+def vote(request, question_id):
+    return HttpResponse(f'Você está votando na pergunta de número {question_id}')
